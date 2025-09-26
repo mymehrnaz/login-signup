a471b3c50b3a1febdd923631f4c21497d0b1754f
mymehrnaz <165458618+mymehrnaz@users.noreply.github.com>
2025-09-26 09:49:40 +0330

    slide changes

--- src/component/login.js
+++ src/component/login.js
@@ -25,11 +25,11 @@ function PreventForm() {
         <h1>Login Form</h1>
         <div className='form'>
             <form onSubmit={handleSubmit}>
-                <label className='lable' for="emailField">
+                <label className='label' htmlFor="emailField">
                     E-mail:</label><br/>
                     <input type="email" pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$' value={value} onChange={handleChange} required /><br/>
 
-                <lable className='lable' for="pwd">Password:</lable> <br/> <input type="password" minLength="6" required /><br/>
+                <lable className='label' htmlFor="pwd">Password:</lable> <br/> <input type="password" minLength="6" required /><br/>
 
                 <button type="submit" className='submit'>Login</button>
             </form>
